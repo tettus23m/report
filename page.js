3faf3e449993953e08b8a142411fa961b98255e3
tettus23m <49706564+tettus23m@users.noreply.github.com>
2019-07-24 15:09:51 +0900

    Update page.js

--- page.js
+++ page.js
@@ -12,7 +12,12 @@ var connection = mysql.createConnection({
 });
 
 server.get('/', function( req, res ) {
-    connection.query('select id, name from race;', (error, rows, fields) => {
+    connection.query('select id, racename, people_id1, people_id2, people_id3 
+                     from result inner join people on 
+                     result.people_id1 = people.id,
+                     result.people_id2 = people.id,
+                     result.people_id3 = people.id
+                     where year = 2019;', (error, rows, fields) => {
         if( error ) {
             console.log('Query Error');
         }
