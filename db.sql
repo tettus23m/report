8f855d8b615f1860c89ac473c6e022bb45d43fe3
tettus23m <49706564+tettus23m@users.noreply.github.com>
2019-07-24 15:33:04 +0900

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,5 +1,5 @@
-create table people ( id int auto_increment not null primary key, name varchar(100) );
-create table race ( id int auto_increment not null primary key, name varchar(100) );
+create table people ( id int auto_increment not null primary key, peoplename varchar(100) );
+create table race ( id int auto_increment not null primary key, racename varchar(100) );
 create table result ( id int auto_increment not null primary key, year int, racename int, people_id1 int, people_id2 int, people_id3 int );
 load data local infile 'people.csv' into table people fields terminated by ',' enclosed by '"';
 load data local infile 'race.csv' into table race fields terminated by ',' enclosed by '"';
