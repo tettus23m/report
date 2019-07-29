bc1efe10ef7a6d8187fab376572f1cea527c0046
tettus23m <49706564+tettus23m@users.noreply.github.com>
2019-07-29 14:57:04 +0900

    Update page.js

--- page.js
+++ page.js
@@ -15,17 +15,7 @@ server.get('/', function( req, res ) {
     res.render( 'sql.ejs', { content: rows });
 });
 
-server.get('/result', function( req, res ) {
-    connection.query('select race.racename, people.peoplename from result 
-                     inner join race on result.racename = race.id 
-                     inner join people on result.people_id1 = people.id
-                     where year = 2019;', (error, rows, fields) => {
-        if( error ) {
-            console.log('Query Error');
-        }
-        res.render( 'sql1.ejs', { content: rows });
-    });
-});
+
 
 server.listen( 80, function() {
     console.log( 'listening on port 80' );
