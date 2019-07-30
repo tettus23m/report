cd341cbf740a44c50354f805393ba121e7e69ec0
tettus23m <49706564+tettus23m@users.noreply.github.com>
2019-07-30 15:55:43 +0900

    Update page.js

--- page.js
+++ page.js
@@ -12,16 +12,16 @@ var connection = mysql.createConnection({
 });
 
 server.get('/', function( req, res ) {
-     let query;
-     connection.query( query, (error, rows, fields) => {
+    connection.query('select race.racename, people.peoplename from result 
+                     inner join race on result.racename = race.id 
+                     inner join people on result.people_id1 = people.id
+                     where year = 2019;', (error, rows, fields) => {
         if( error ) {
             console.log('Query Error');
         }
         res.render( 'sql.ejs', { content: rows });
     });
 });
-
-
 
 server.listen( 80, function() {
     console.log( 'listening on port 80' );
